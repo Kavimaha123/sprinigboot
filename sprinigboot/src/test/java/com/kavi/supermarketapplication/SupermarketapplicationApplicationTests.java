a3568dbb5e88472004b5bfeeead39872f7baec2e
vijeyender <vijayvijeyender@gmail.com>
2023-07-23 10:06:38 +0530

    vijay

---
+++ sprinigboot/src/test/java/com/kavi/supermarketapplication/SupermarketapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavi.supermarketapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
